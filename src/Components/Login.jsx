a977715e55330be4a9a3435107695902fbeb44fd
syam <gsyamkumar96@gmail.com>
2024-11-11 08:36:01 +0530

    name added

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -4,8 +4,9 @@ export const Login = () => {
   return (
     <div>
       <h1>Welcome To Login Page...!</h1>
-          <p>Welcome</p>
-          <h3>Ecoomerce</h3>
+      <p>Welcome</p>
+      <h3>Ecoomerce</h3>
+      <h1>Syam Kumar...</h1>
     </div>
   );
 };
